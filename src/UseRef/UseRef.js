ce99e0dfeaf30f11272855406ce38a15b1c2610b
MkMonir <khanmonir1335@gmail.com>
2023-03-02 15:36:03 +0600

    learn useRef hook

---
+++ src/UseRef/UseRef.js
@@ -0,0 +1,16 @@
+import { useRef } from "react";
+
+const UseRef = () => {
+  const inputRef = useRef(null);
+
+  return (
+    <div style={{ display: "flex", gap: "5px", alignItems: "center" }}>
+      <input className="input" placeholder="Type something" ref={inputRef} />
+      <button className="btn" onClick={() => inputRef.current.focus()}>
+        Click
+      </button>
+    </div>
+  );
+};
+
+export default UseRef;
