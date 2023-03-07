da912fbe160f27ad8cc9d03fba27a211d810221e
MkMonir <khanmonir1335@gmail.com>
2023-03-07 14:10:56 +0600

    create custom useDoumentTitle hook

---
+++ src/CustomHooks/useDocumentTitle.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+const useDocumentTitle = (title) => {
+  useEffect(() => {
+    const prevTitle = document.title;
+    document.title = title;
+
+    return () => {
+      document.title = prevTitle;
+    };
+  }, [title]);
+
+  return null;
+};
+
+export default useDocumentTitle;
